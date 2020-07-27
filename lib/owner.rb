3f3cd4c4c1ddd4a681c23bf710ace48be0039931
William Han Findiesen <william.findiesen@gmail.com>
2020-07-27 00:04:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
 class Owner
   # code goes here
+  #attr_accessor :name 
+  @@all = []
+  @@owners = []
+  @@pets = {:dogs => [], :cats => []}
+  
+  attr_reader :species, :name
+  def initialize(name)
+   @name = name 
+   @species = "human" 
+   save
+   @@owners << self
+  end 
+  def say_species
+     "I am a #{@species}."
+  end 
+  def save
+    @@all << self
+  end 
+  def self.all 
+    @@all << self 
+  end 
+  def self.count
+    @@owners.length 
+  end 
+  def self.reset_all
+    @@owners = []
+  end 
+  
+  def cats
+    @@pets.select {|pets| pets.Class == Cat}
+  end
+  
+  def buy_cat
+    new_cat = Cat.new
+  end
 end
+  
+
+   
